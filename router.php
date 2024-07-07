0d009b2b4d59596d98fd73bc54edb2d059e4a8ca
moonydog12 <losididog@gmail.com>
2024-07-07 10:59:17 +0800

    新增 note-create相關服務

--- router.php
+++ router.php
@@ -1,13 +1,7 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/notes' => 'controllers/notes.php',
-  '/note' => 'controllers/note.php',
-  '/contact' => 'controllers/contact.php',
-];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$routes = require('routes.php');
 
 function abort($code = 404)
 {
