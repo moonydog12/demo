f4c0e389cd2fa6e573474d49c86cfe05b505d728
Jhih Luo <losididog@gmail.com>
2024-07-08 10:30:34 +0800

    簡易service container(框架會提供更強的功能)

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,11 +1,11 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-$currentUserId = 1;
+$db = App::resolve(Database::class);
 
+$currentUserId = 1;
 
 $note = $db->query(
   'SELECT * FROM notes WHERE id = :id',
