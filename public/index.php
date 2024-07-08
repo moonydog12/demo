ec504f17a4e9b6efdd688a914ab9df1c85192690
Jhih Luo <losididog@gmail.com>
2024-07-08 11:21:33 +0800

    新增update note功能(完成簡易CRUD)

--- public/index.php
+++ public/index.php
@@ -11,6 +11,8 @@ spl_autoload_register(function ($class) {
   require base_path("{$class}.php");
 });
 
+require base_path('bootstrap.php');
+
 $router = new \Core\Router();
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
