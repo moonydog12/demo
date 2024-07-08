781b2ec5734dad800fcb60651e83267f1c62c7f6
Jhih Luo <losididog@gmail.com>
2024-07-08 17:35:37 +0800

    新增 registration 及對應 controller

---
+++ controllers/registration/store.php
@@ -0,0 +1,52 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// validate the form inputs.
+$errors = [];
+if (!Validator::email($email)) {
+  $errors['email'] = 'Please provide an valid email address.';
+}
+
+if (!Validator::string($password, 7, 255)) {
+  $errors['password'] = 'Please provide a password of at least seven characters.';
+}
+
+if (!empty($errors)) {
+  return view('registration/create.view.php', [
+    'errors' => $errors,
+  ]);
+}
+
+
+$db = App::resolve(Database::class);
+// check if the account already exists
+$user = $db->query('SELECT * FROM users WHERE email = :email', [
+  'email' => $email,
+])->find();
+
+if ($user) {
+  // then someone with that email already exists and has an account.
+  // if yes, redirect to  login page.
+  header('location: /');
+  exit();
+} else {
+  // if not, save one to the database, and then log the user in, and redirect
+  $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+    'email' => $email,
+    'password' => $password
+  ]);
+
+  // mark that the user has logged in
+  $_SESSION['user'] = [
+    'email' => $email
+  ];
+
+  header('location: /');
+  exit();
+}
